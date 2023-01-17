26571504cb0e73462f13eff4d222c93bc6a50132
Billy <learnlinuxtv@gmail.com>
2023-01-17 20:15:21 +0200

    Comments and Style 05

---
+++ comments_and_style_5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+print("This program will fail.")
+
+# It will fail because variable names shouldn't have spaces.
+
+my var1 = 3
+myvar2 = 2
+myvar1 = 5
+
+print(myvar1)
+print(myvar2)
---
+++ comments_and_style_6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+print("This program will not fail.")
+
+# But myvar1 will be equal to 5, because initially was declared equal to 3
+#and next equal to 5, it will keep the last value that it was set to.
+
+myvar1 = 3
+myvar2 = 2
+myvar1 = 5
+
+print(myvar1)
+print(myvar2)
