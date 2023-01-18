9fbe290d47be12cd1ec89e3b5395f5c9334dcb47
Billy <learnlinuxtv@gmail.com>
2023-01-18 08:27:07 +0200

    Add a doc string.

--- functions_03.py
+++ functions_03.py
@@ -2,6 +2,11 @@
 
 
 def my_function():
+    """This is a doc string.
+    This is useful and 
+    we should use it in functions
+    to explain their functionality
+    """
     mynum1 = 1000000
     mynum2 = 3077878
     print(mynum1 + mynum2)
