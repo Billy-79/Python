b579c2e11552e55942c838a3e0b1f0205eef6605
Billy <learnlinuxtv@gmail.com>
2023-01-18 19:08:12 +0200

    Use if, elif and else to capture a range of the age variable that was entered by the user.

---
+++ if_statements_07.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+age = input("Please enter your age: ")
+
+if int(age) >= 50:
+    print("You are 50 or older.")
+elif int(age) >= 18:
+    print("You are an adult.")
+else:
+    print("You are not even an adult.")
+
+print("This will always print.")
