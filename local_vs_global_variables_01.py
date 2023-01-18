392d0486c5dcafa7cf576254ea6842f5298f03b2
Billy <learnlinuxtv@gmail.com>
2023-01-18 08:46:10 +0200

    A variable declared within a function it doesn't exist outside the scope of the function.

---
+++ local_vs_global_variables_01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def my_function():
+    """This is a sample function.
+    We're going to declare a variable 
+    inside this function.
+    """
+    message = "Hello World!"
+    print(message)
+
+my_function()
+
+print(message)
