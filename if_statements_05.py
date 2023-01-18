4ccff8707c9731974060b359f9bf8a356218e637
Billy <learnlinuxtv@gmail.com>
2023-01-18 18:49:28 +0200

    Ask user to enter a number between 1 and 10 - though this is not the best practice.

---
+++ if_statements_05.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+number = input("Please enter a number between 1 and 10: ")
+
+if int(number) <= 10:
+    print("A valid number was entered.")
+
+print("This will always print.")
