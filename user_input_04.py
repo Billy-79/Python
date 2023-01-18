3e9e4135db17ce736ca7cc0355dbda784abe0d64
Billy <learnlinuxtv@gmail.com>
2023-01-18 09:31:34 +0200

    The use of converting data types.

---
+++ user_input_04.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+age = input("Enter your age:\n")
+print("You are " + age + " years old.")
+
+age2 = int(age) + 50
+
+print("In 50 years, you will be " + str(age2) + " years old.")
