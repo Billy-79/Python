d06cf48895b8bac05878930275e8940bb86c2bef
Billy <learnlinuxtv@gmail.com>
2023-01-19 11:23:03 +0200

    Tupal is similar to list but immutable

---
+++ tuples_02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+heroes_tuple = ("The Batman", "Spider-Man", "The Joker", "Wonder Woman")
+heroes_list = ["The Batman", "Spider-Man", "The Joker", "Wonder Woman"]
+
+print(heroes_tuple)
+print(heroes_list)
