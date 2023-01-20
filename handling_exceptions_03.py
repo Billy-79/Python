a236a79788aa04ad759c94165aaa7fe6518410eb
Billy <learnlinuxtv@gmail.com>
2023-01-20 20:30:16 +0200

    Handling exceptions - simulating to start a service which isn't present.

---
+++ handling_exceptions_03.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+import subprocess
+
+svc = "sshd"
+
+check_cmd = ["ps",
+             "-C",
+             svc]
+
+service_check = subprocess.call(check_cmd)
+
+if service_check == 0:
+    print("The service is running.")
+else:
+    print("The service is NOT running.")
+    print("Starting it...")
+    try:
+        subprocess.call(["systemctl", "start", "sshdd"])
+    except:
+        print("There was an error starting the process.")
+        exit(1)
+    subprocess.call(check_cmd)
