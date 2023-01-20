4bfd677eb5ed5453d8fb27094c3d3cb3d0b5f838
Billy <learnlinuxtv@gmail.com>
2023-01-20 19:11:10 +0200

    Subprocess module - check if a service is running.

---
+++ subprocess_01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+import subprocess
+
+svc = "sshd"
+
+service_check = subprocess.call(["ps", "-C", svc])
+
+if service_check == 0:
+    print("The service is running.")
+else:
+    print("The service is NOT running.")
