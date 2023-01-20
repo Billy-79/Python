1d9425c62eb71e237331c907c371dcfc4403415f
Billy <learnlinuxtv@gmail.com>
2023-01-20 18:49:57 +0200

    os module - checking the existence of a file and creating it if it doesn't exist.

---
+++ more_os_module_03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+import os
+
+file = input("Enter a filename: ")
+
+if os.path.isfile(file):
+    print("The file exists.")
+else:
+    print("The file does not exist.")
+    print("Creating it...")
+    os.system("touch {}".format(file))
