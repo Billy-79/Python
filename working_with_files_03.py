3ea8c4216f578bcebe14a82a843177cd0c450a3c
Billy <learnlinuxtv@gmail.com>
2023-01-22 12:50:45 +0200

    working with files - open a file in append mode and write to it.

---
+++ working_with_files_03.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+myfile = "file1.txt"
+try:
+    file = open(myfile, "a")
+except FileNotFoundError as e:
+    print("The file was not found.")
+    print(e)
+    exit(1)
+
+movies = ["The Matrix, ", "The Lord of the Rings, ", "The Avengers - "]
+
+for m in movies:
+    file.write(m)
+file.close()
