415736cbf067355e7351ec01c6c2e7966b897e3c
Billy <learnlinuxtv@gmail.com>
2023-01-22 19:32:18 +0200

    Using Flask and render_template to serve an html page (site.html also needed inside a directory named templates)

---
+++ website_03.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def html_page():
+    return render_template("site.html")
+
+app.run(debug=True)
