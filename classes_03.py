fa64dea3426fb26dc734ce35e1e09c101fabd535
Billy <learnlinuxtv@gmail.com>
2023-01-23 19:04:31 +0200

    Classes 03 - a game scenario.

---
+++ classes_03.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+class GameCharacter:
+    def __init__(self, name, life):
+        self.name = name
+        self.life = life
+
+    def attack(self):
+        print(self.name + " kicks the enemy.")
+
+class GameEnemy:
+    def __init__(self, name, life):
+        self.name = name
+        self.life = life
+
+    def attack(self):
+        print(self.name + " attacks the heroes!")
+
+
+player1 = GameCharacter("Roger", 100)
+player2 = GameCharacter("Betty", 100)
+enemy1 = GameEnemy("Bill", 150)
+enemy2 = GameEnemy("Bob", 150)
+
+
+player1.attack()
+enemy1.life -= 50
+print("Bill takes 50 damage, and has " + str(enemy1.life) + " life remaining.")
+
+player2.attack()
+enemy2.life -= 70
+print("Bob takes 70 damage, and has " + str(enemy2.life) + " life remaining.")
